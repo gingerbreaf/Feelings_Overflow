3fce4c2be18cd5fa7163c75e7567da3c02ce0b1f
Jiang Bowei <jiangbowei@Gingerrr.local>
2023-05-27 15:28:35 +0800

    testing

--- lib/main.dart
+++ lib/main.dart
@@ -64,7 +64,7 @@ class _MyHomePageState extends State<MyHomePage> {
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
+      _counter--;
     });
   }
 
@@ -118,7 +118,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.exposure_minus_1),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
